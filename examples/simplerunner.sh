6657972737127853736607739225262db6ece5d5
Niko Junkala <baabelfish@gmail.com>
2013-10-14 07:32:56 +0300

    Made it simpler

--- examples/simplerunner.sh
+++ examples/simplerunner.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 TESTS='tests'
-TESTBIN='.tests'
+TESTBIN='tests/.test'
 FLAGS='-g -std=c++11 -I./include'
 CC='g++'
 
 ################################################################################
-[[ ! -d "$TESTBIN" ]] && mkdir "$TESTBIN"
 TESTS=($(find $TESTS -type f -name '*.cpp'|grep -v 'tests/utest/examples'))
 NUMBER=1
 
-for i in ${TESTS[@]}; do
-    $CC $FLAGS -o $TESTBIN/test$NUMBER $i
-    NUMBER=$((NUMBER+1))
-done
+clear
 
-for (( i = 1; i < $NUMBER; i++ )); do
-    $TESTBIN/test$i
+for i in ${TESTS[@]}; do
+    $CC $FLAGS -o $TESTBIN $i $(grep '// UTESTINC: ' $i|cut -f2 -d ':')
+    $1 $TESTBIN
     echo ""
-    rm -f $TESTBIN/test$i
 done
