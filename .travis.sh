128ce0e75aa20b4664320c9229dfe9771e77d0e1
Niko Junkala <baabelfish@gmail.com>
2014-05-08 21:25:07 +0300

    Another test

--- .travis.sh
+++ .travis.sh
@@ -1,5 +1,4 @@
 sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test
 sudo apt-get -qq update
-sudo apt-get install llvm
-sudo apt-get -qq install libstdc++-4.8-dev
-clang++ --version && make && ./utests
+# sudo apt-get install llvm
+make && ./utests
